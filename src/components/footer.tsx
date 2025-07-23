bd52586312891993294f0389aab0da7defea7360
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2025-07-23 17:13:40 +0200

    feat/ footer update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 
 export const Footer = () => (
   <footer className="flex justify-between items-center p-4">
-    <p className="text-xl font-bold">Footer</p>
+    <p className="text-xl font-bold">Łukasz Kaliszewicz lukasz.kaliszewicz01@gmail.com</p>
   </footer>
 );
