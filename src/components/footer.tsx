364a9627f240e9e594abf6cab4379e5adaa44596
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2025-08-02 22:47:17 +0200

    fix/ tailwind fixes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 
 export const Footer = () => (
-  <footer className="2xl:w-[85rem] lg:w-[100%] xl:w-[90%] w-full flex flex-col lg:flex-row justify-between items-center p-4 mx-auto">
+  <footer className="2xl:w-[85rem] lg:w-[100%] xl:w-[90%] w-full flex flex-col lg:flex-row justify-between items-center px-4 pt-10 pb-18 gap-4 mx-auto">
     <p className="text-xl font-bold max-w-fit ">Łukasz Kaliszewicz</p>
     <p className="text-xl font-bold max-w-fit ">lukasz.kaliszewicz01@gmail.com</p>
   </footer>
